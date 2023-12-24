f6993fbf00b081a3c19b34cf50b508bd827a6d7b
guohaolu <1670212878@qq.com>
2023-12-24 19:42:16 +0800

    commit

---
+++ src/test/java/com/stealandfire/demo/thread/RecursiveDemoTask.java
@@ -0,0 +1,12 @@
+package com.stealandfire.demo.thread;
+
+import java.util.concurrent.RecursiveTask;
+
+public class RecursiveDemoTask extends RecursiveTask {
+    private static final long serialVersionUID = -3577644910069163975L;
+
+    @Override
+    protected Object compute() {
+        return null;
+    }
+}
